5118ff9aed6085282095c035a6274d36d9093569
Yunseok Kang <mok02198@gmail.com>
2022-04-09 19:26:06 +0900

    Initial commit

---
+++ src/test/java/com/kys95/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kys95.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
