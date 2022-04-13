b0703e66d833e3044741720dd6e82a737575bb27
Yunseok Kang <mok02198@gmail.com>
2022-04-13 23:22:59 +0900

    Create SecurityConfig.java

---
+++ src/main/java/com/kys95/jwt/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.kys95.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //session 사용x
+                .and()
+                .addFilter(corsFilter)  //@CrossOrigin(인증없는 경우), security filter에 등록(인증있는 경우)
+                .formLogin().disable()  //jwt 서버이므로 formLogin 사용x
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/usr/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+
+    }
+}
