2007a5e4636c66ebc1f04e815cd46a525c0b68d0
Yunseok Kang <mok02198@gmail.com>
2022-04-14 00:31:43 +0900

    Create PrincipalDetailsService.java

---
+++ src/main/java/com/kys95/jwt/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,25 @@
+package com.kys95.jwt.config.auth;
+
+import com.kys95.jwt.model.User;
+import com.kys95.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * http://localhost:8080/login -> loginForm disable했으므로 여기서 동작을 안함
+ */
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        return new PrincipalDetails(userEntity);
+    }
+}
