d409d6ca7c62f3345586c1b72c57df461de3527b
Yunseok Kang <mok02198@gmail.com>
2022-04-14 00:31:54 +0900

    Create UserRepository.java

---
+++ src/main/java/com/kys95/jwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kys95.jwt.repository;
+
+import com.kys95.jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByUsername(String username);
+}
