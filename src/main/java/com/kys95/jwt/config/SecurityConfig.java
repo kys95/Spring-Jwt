6c957f5eebfac90f59d73b16dc68690b7df85c0f
Yunseok Kang <mok02198@gmail.com>
2022-04-14 16:35:11 +0900

    Update SecurityConfig.java

--- src/main/java/com/kys95/jwt/config/SecurityConfig.java
+++ src/main/java/com/kys95/jwt/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.kys95.jwt.config;
 
 import com.kys95.jwt.config.jwt.JwtAuthenticationFilter;
+import com.kys95.jwt.config.jwt.JwtAuthorizationFilter;
+import com.kys95.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,25 +17,26 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CorsFilter corsFilter;
+    @Autowired
+    private CorsConfig corsConfig;
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private UserRepository userRepository;
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //session 사용x
+        http.addFilter(corsConfig.corsFilter())  //@CrossOrigin(인증없는 경우), security filter에 등록(인증있는 경우)
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //session 사용x
                 .and()
-                .addFilter(corsFilter)  //@CrossOrigin(인증없는 경우), security filter에 등록(인증있는 경우)
                 .formLogin().disable()  //jwt 서버이므로 formLogin 사용x
                 .httpBasic().disable()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))   //AuthenticationManager
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
                 .authorizeRequests()
                 .antMatchers("/api/v1/usr/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
