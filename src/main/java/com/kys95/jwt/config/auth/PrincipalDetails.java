31c41b945103ca4496a36806169b1fa21e65c665
Yunseok Kang <mok02198@gmail.com>
2022-04-14 14:14:56 +0900

    Update PrincipalDetails.java

--- src/main/java/com/kys95/jwt/config/auth/PrincipalDetails.java
+++ src/main/java/com/kys95/jwt/config/auth/PrincipalDetails.java
@@ -2,12 +2,14 @@ package com.kys95.jwt.config.auth;
 
 
 import com.kys95.jwt.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Data
 public class PrincipalDetails implements UserDetails {
 
     private User user;
