2ac59a09478f893d18db26b2cf8c2f82e1181403
Yunseok Kang <mok02198@gmail.com>
2022-04-14 16:35:01 +0900

    Update RestApiController.java

--- src/main/java/com/kys95/jwt/controller/RestApiController.java
+++ src/main/java/com/kys95/jwt/controller/RestApiController.java
@@ -4,10 +4,7 @@ import com.kys95.jwt.model.User;
 import com.kys95.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -29,5 +26,23 @@ public class RestApiController {
         user.setRoles("ROLE_USER");
         userRepository.save(user);
         return "회원가입 완료";
+    }
+
+    //user,manager,admin 권한 접근 가능
+    @GetMapping("api/v1/user")
+    public String user(){
+        return "user";
+    }
+
+    //manager, admin 권한 접근 가능
+    @GetMapping("api/v1/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    //admin 권한 접근 가능
+    @GetMapping("api/v1/admin")
+    public String admin(){
+        return "admin";
     }
 }
