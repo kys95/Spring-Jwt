3ed27f74c52ca7d1828d474ca42fea0c6c54025e
Yunseok Kang <mok02198@gmail.com>
2022-04-14 14:15:18 +0900

    Update SecurityConfig.java

--- src/main/java/com/kys95/jwt/config/SecurityConfig.java
+++ src/main/java/com/kys95/jwt/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.kys95.jwt.config;
 import com.kys95.jwt.config.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -16,6 +18,11 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsFilter corsFilter;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
