58f2c6c40d89d07d9f5073ec52eabcca74efb2a3
Yunseok Kang <mok02198@gmail.com>
2022-04-14 14:15:07 +0900

    Update RestApiController.java

--- src/main/java/com/kys95/jwt/controller/RestApiController.java
+++ src/main/java/com/kys95/jwt/controller/RestApiController.java
@@ -1,19 +1,33 @@
 package com.kys95.jwt.controller;
 
+import com.kys95.jwt.model.User;
+import com.kys95.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class RestApiController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("home")
     public String home(){
         return "<h1>home</h1>";
     }
 
-    @PostMapping("token")
-    public String token(){
-        return "<h1>token</h1>";
+    @PostMapping("join")
+    public String join(@RequestBody User user){
+
+        String lawPassword = user.getPassword();
+        user.setPassword(bCryptPasswordEncoder.encode(lawPassword));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입 완료";
     }
 }
